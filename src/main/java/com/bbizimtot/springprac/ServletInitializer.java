55ac8e6b51741433356ec1a3c74b28819402e296
CJH\bbizi <bbizimtot@naver.com>
2024-02-16 14:59:21 +0900

    First commit!

---
+++ src/main/java/com/bbizimtot/springprac/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bbizimtot.springprac;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringPracApplication.class);
+	}
+
+}
