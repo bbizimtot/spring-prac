b39fc4c1b25f3749f18bc2ab2a615f30aa89b453
bbizimtot <38424899+bbizimtot@users.noreply.github.com>
2024-02-16 15:43:07 +0900

    Third

--- src/main/java/com/bbizimtot/springprac/HelloController.java
+++ src/main/java/com/bbizimtot/springprac/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/")    
     public String index() {
-        return "Hello, Spring Boot!!!";
+        return "Hello, Spring Boot!!! 123";
     }
 }
